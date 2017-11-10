1ed8813dbb8872ca41b14a66364829d4917b458c
ethomm <eirik@meetprod.no>
2017-11-10 23:31:26 +0100

    database stuff

--- db/migrate/20171110185127_add_contact_id_to_globalsettings.rb
+++ db/migrate/20171110185127_add_contact_id_to_globalsettings.rb
@@ -1,5 +1,5 @@
 class AddContactIdToGlobalsettings < ActiveRecord::Migration[5.1]
   def change
-    add_column :globalsettings, :contact_id, :string
+    add_column :globalsettings, :contact_id, :integer , :default => 0
   end
 end
