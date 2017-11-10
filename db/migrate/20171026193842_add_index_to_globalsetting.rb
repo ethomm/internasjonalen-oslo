daca6a0193c79d7fb2a0c8d9ff4b1291806b1e90
ethomm <eirik@meetprod.no>
2017-11-10 23:08:24 +0100

    setup

--- db/migrate/20171026193842_add_index_to_globalsetting.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToGlobalsetting < ActiveRecord::Migration[5.1]
-  def change
-  	add_index :globalsettings, :singleton_guard, :unique => true
-  end
-end
--- db/migrate/20171110185127_add_contact_id_to_globalsettings.rb
+++
@@ -1,5 +0,0 @@
-class AddContactIdToGlobalsettings < ActiveRecord::Migration[5.1]
-  def change
-    add_column :globalsettings, :contact_id, :string
-  end
-end
