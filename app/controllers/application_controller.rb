78adf773aaf5aeec7b1ed2d9b7f8435ee4123576
ethomm <eirik@meetprod.no>
2017-12-13 14:26:20 +0100

    updateing helping info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_stuff
   include SessionsHelper
+
+  private	
+  	def set_stuff
+  		@setting = Globalsetting.first
+      if @setting.contact_id != 0;
+        @contactperson = Contactperson.find(@setting.contact_id)
+      end
+      if @setting.booking_id != 0;
+        @bookingperson = Contactperson.find(@setting.booking_id)
+      end
+      if @setting.teknisk_id != 0;
+        @tekniskperson = Contactperson.find(@setting.teknisk_id)
+      end
+      @categories = Category.all
+      @bookings = Booking.all
+      @about = Omoss.first
+      @bars = Bar.all
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-  before_action :set_stuff
   def home
   end
 
@@ -13,22 +12,4 @@ class PagesController < ApplicationController
   def tekniskespsifikasjoner
     @tekniskcategories = Tekniskcategory.all.eager_load(:tekniskitems)
   end
-  
-  private	
-  	def set_stuff
-  		@setting = Globalsetting.first
-      if @setting.contact_id != 0;
-        @contactperson = Contactperson.find(@setting.contact_id)
-      end
-      if @setting.booking_id != 0;
-        @bookingperson = Contactperson.find(@setting.booking_id)
-      end
-      if @setting.teknisk_id != 0;
-        @tekniskperson = Contactperson.find(@setting.teknisk_id)
-      end
-      @categories = Category.all
-      @bookings = Booking.all
-      @about = Omoss.first
-      @bars = Bar.all
-    end
 end
