667a7495ecdc1fa4543886a2d9577d0ea04f8323
ethomm <eirik@meetprod.no>
2018-04-19 16:03:27 +0200

    refactored code

--- app/assets/javascripts/internasjonalen.js
+++ app/assets/javascripts/internasjonalen.js
@@ -1,6 +1,6 @@
 
 var ready;
-
+var map;
 
 
 ready = function (){
@@ -114,7 +114,7 @@ $('#newFrontpageImage').on('shown.bs.modal', function () {
 }
 
 //$( document ).on('turbolinks:load', ready());
-$(window).bind('turbolinks:load', ready)
+
 // Function to check seodescriptions
 
 function seodescription(){
@@ -150,14 +150,10 @@ function initMap(){
   }else{
     console.log('No map to setup')
   }
-
 }
 
-function setMap() {
+function setMap(){
   var grevent = {lat: 59.914539531265376, lng: 10.749528244163002};
-  
-
-
   var myOptions = {
     zoom: 17,
     center: grevent,
@@ -169,200 +165,68 @@ function setMap() {
 
     };
 
-    var styledMapType = new google.maps.StyledMapType(
+  var styledMapType = new google.maps.StyledMapType(
       [
-        {
+        {"elementType": "geometry",
+          "stylers": [{"color": "#f5f5f5"}]},
+        {"elementType": "labels.icon",
+          "stylers": [{"visibility": "off"}]},
+        {"elementType": "labels.text.fill",
+          "stylers": [{"color": "#616161"}]},
+        {"elementType": "labels.text.stroke",
+          "stylers": [{"color": "#f5f5f5"}]},
+        {"featureType": "administrative",
           "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
+          "stylers": [{"visibility": "off"}]},
+        {"featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#bdbdbd"}]},
+        {"featureType": "poi",
+          "stylers": [{"visibility": "off"}]},
+        {"featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [{"color": "#eeeeee"}]},
+        {"featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#757575"}]},
+        {"featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [{"color": "#e5e5e5"}]},
+        {"featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#9e9e9e"}]},
+        {"featureType": "road",
+          "elementType": "geometry",
+          "stylers": [{"color": "#ffffff"}]},
+        {"featureType": "road",
           "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
+          "stylers": [{"visibility": "off"}]},
+        {"featureType": "road.arterial",
           "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative",
+          "stylers": [{"color": "#757575"}]},
+        {"featureType": "road.highway",
           "elementType": "geometry",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.land_parcel",
+          "stylers": [{"color": "#dadada"}]},
+        {"featureType": "road.highway",
           "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#bdbdbd"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
+          "stylers": [{"color": "#616161"}]},
+        {"featureType": "road.local",
           "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
+          "stylers": [{"color": "#9e9e9e"}]},
+        {"featureType": "transit",
+          "stylers": [{"visibility": "off"}]},
+        { "featureType": "transit.line",
           "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
+          "stylers": [{"color": "#e5e5e5"}]},
+        { "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [{"color": "#eeeeee"}]},
+        { "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{"color": "#c9c9c9"}]},
+        { "featureType": "water",
           "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#ffffff"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#dadada"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "transit",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#c9c9c9"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        }
+          "stylers": [{"color": "#9e9e9e"}]}
       ],
         {name: 'Styled Map'});
     
@@ -379,14 +243,10 @@ function setMap() {
   map.setMapTypeId('styled_map');
 
   marker.addListener('click', function() {
-      console.log('marker click');
-      var url = 'https://www.google.com/maps/place/Internasjonalen/@59.9141892,10.7487952,419m/data=!3m1!1e3!4m13!1m7!3m6!1s0x0:0x0!2zNDXCsDI3JzUxLjAiTiA5wrAxMScyOS4wIkU!3b1!8m2!3d45.464167!4d9.191389!3m4!1s0x0:0x2f311a016cc4580a!8m2!3d59.9145319!4d10.7495843?hl=no';
-        window.open(url, "_blank")
-
-        });
-
+    var url = 'https://www.google.com/maps/place/Internasjonalen/@59.9141892,10.7487952,419m/data=!3m1!1e3!4m13!1m7!3m6!1s0x0:0x0!2zNDXCsDI3JzUxLjAiTiA5wrAxMScyOS4wIkU!3b1!8m2!3d45.464167!4d9.191389!3m4!1s0x0:0x2f311a016cc4580a!8m2!3d59.9145319!4d10.7495843?hl=no';
+    window.open(url, "_blank")
+    });
 }
 
-
-
+$(window).bind('turbolinks:load', ready)
 
